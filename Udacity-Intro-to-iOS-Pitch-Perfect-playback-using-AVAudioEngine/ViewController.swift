bce689f8899bd246f7a97f9f8b6334f45fb2bffa
programaudio <User@users-mbp.attlocal.net>
2015-11-22 02:20:43 -0600

    added resource files
    Added .mp3 audio file to be played back.
    
    Added button and app icon images.
    
    Added actions for the buttons.
    
    Started to add the AVAudioEngine logic into the main viewController.

--- Udacity-Intro-to-iOS-Pitch-Perfect-playback-using-AVAudioEngine/ViewController.swift
+++ Udacity-Intro-to-iOS-Pitch-Perfect-playback-using-AVAudioEngine/ViewController.swift
@@ -5,14 +5,44 @@
 //  Created by User on 11/21/15.
 //  Copyright © 2015 John Carlson. All rights reserved.
 //
-
+//
 import UIKit
-
+//
+import AVFoundation
+//
+//
+// http://www.rockhoppertech.com/blog/swift-avfoundation/
+//
+//
 class ViewController: UIViewController {
+    //
+    //
+    //
+    var audioEngine: AVAudioEngine
+    //
+    var audioPlayerNode: AVAudioPlayerNode
+    //
+    let movieQuoteAudioFileURL = NSBundle.mainBundle().URLForResource("movie_quote", withExtension: "mp3") // -> NSURL?
+    //
+    //
+    var movieQuoteAudioFile: AVAudioFile
+    
+    
+    //
+    //
     //
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //
+        if(movieQuoteAudioFileURL != nil){
+            do{
+                try movieQuoteAudioFile = AVAudioFile(forReading: movieQuoteAudioFileURL!)
+            }
+            catch{
+                //
+            }
+        }
     }
     //
     override func didReceiveMemoryWarning() {
@@ -20,5 +50,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     //
+    //
+    @IBAction func playbackSlow(sender: UIButton) {
+    }
+    //
+    //
+    @IBAction func playbackFast(sender: UIButton) {
+    }
+    //
+    //
+    @IBAction func stopPlayback(sender: UIButton) {
+    }
 }
 
