5203ccec04c7e4c6224d1fb25071fd5ba238d285
programaudio <User@users-mbp.attlocal.net>
2015-11-21 18:12:23 -0600

    base Xcode iOS single view project template
    Added the files that Xcode generated from the iOS single view app
    template.
    
    Loaded using the GitHub desktop app.

---
+++ Udacity-Intro-to-iOS-Pitch-Perfect-playback-using-AVAudioEngine/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Udacity-Intro-to-iOS-Pitch-Perfect-playback-using-AVAudioEngine
+//
+//  Created by User on 11/21/15.
+//  Copyright © 2015 John Carlson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    //
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //
+}
+
